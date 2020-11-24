d38abb9f72d507c5276cf5dad35278628da9abc0
DESKTOP-R9MVMQ4\Emanuela <Grigorie.tkdcf@gmail.com>
2020-11-24 17:55:17 +0200

    Versiunea unde se afiseaza ca cartofi nu se pot colecta in luna a 4-a

--- CultivareaCartofilor/Lectia5/Program.cs
+++ CultivareaCartofilor/Lectia5/Program.cs
@@ -6,7 +6,15 @@ namespace Lectia5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("In ce luna vei cultiva cartofii?");
+            int laCeLunaVaGanditi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nu este posibil ca cartifii sa fie cultivati in aceasta luna");
+
+            Console.WriteLine("Daca au fost saditi in lua a 3 - apasa A");
+            Console.WriteLine("Daca au fost saditi in luna a 4 apasa B");
+            string operatia = Console.ReadLine();
+
+
         }
     }
 }
