5a329f5a82145cdc15eccd31cd26d50bf8498f59
DESKTOP-R9MVMQ4\Emanuela <Grigorie.tkdcf@gmail.com>
2020-11-28 12:30:44 +0200

    Lectia 5 programul corectat cu Victor impreuna

--- CultivareaCartofilor/Lectia5/Program.cs
+++ CultivareaCartofilor/Lectia5/Program.cs
@@ -7,24 +7,10 @@ namespace Lectia5
         static void Main(string[] args)
         {
             Console.WriteLine("In ce luna ati sadit cartofii?");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int luna = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("pentru a afla perioada de recoltare apasa tasta A");
-            
-            string luna = Console.ReadLine();
-            string A = Console.ReadLine();
-            
-            if (luna == "A")
-            {
-                int suma = a  + 4;
-                Console.WriteLine("Vesti putea cultiva peste " + suma );
-            
-            
-            }
-
-            
-
-
+            int lunaRecoltei = luna + 4;
+            Console.WriteLine("Vesti putea recolta in luna a " + lunaRecoltei);
         }
     }
 }
