923eb886ab69a7323a5ea255c655dc88da6778b7
DESKTOP-R9MVMQ4\Emanuela <Grigorie.tkdcf@gmail.com>
2020-12-04 18:26:08 +0200

    Rezolvarea temei pentru acasa  cu afisarea perioadei de recolta cu litere folosind instructiunea If

--- CultivareaCartofilor/Lectia5/Program.cs
+++ CultivareaCartofilor/Lectia5/Program.cs
@@ -6,11 +6,62 @@ namespace Lectia5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("In ce luna ati sadit cartofii?");
+            Console.WriteLine("In ce luna ati cultivat cartofii?");
             int luna = Convert.ToInt32(Console.ReadLine());
+          
+            if(luna == 1)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Mai");
+            }
+            if (luna ==2 )
+            {
+                Console.WriteLine("Veti putea recolta cartofii in iunie");
+            }
+            if (luna == 3)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Iulie");
+            }
+            if (luna == 4)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in August");
+            }
+            if (luna == 5)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Septembrie");
+            }
+            if (luna == 6)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Octombrie");
+            }
+            if (luna == 7)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Noiembrei");
+            }
+            if (luna == 8)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Decembrie");
+            }
+            if (luna == 9)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Ianuarie");
+            }
+            if (luna == 10)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Februarie");
+            }
+            if (luna == 11)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Martie");
+            }
+            if (luna == 12)
+            {
+                Console.WriteLine("Veti putea recolta cartofii in Aprilie");
+            }
 
-            int lunaRecoltei = luna + 4;
-            Console.WriteLine("Vesti putea recolta in luna a " + lunaRecoltei);
+            Console.WriteLine("Press a key to exit...");
         }
+
+
     }
 }
+              
